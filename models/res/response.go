2f7f957026345e7c96d5a6beee573d94cdcf7bfd
Fu zhicheng <fuzhicheng@guanxingtuan.top>
2023-10-08 18:29:57 +0800

    feat.状态码完善

--- models/res/response.go
+++ models/res/response.go
@@ -11,47 +11,42 @@ type Response struct {
 	Msg  string
 }
 
-const (
-	SUCCESS int = 0
-	Error       = 7
-)
-
-func Result(code int, data any, msg string, c *gin.Context) {
+func Result(code Code, data any, msg string, c *gin.Context) {
 	c.JSON(http.StatusOK, Response{
-		Code: code,
+		Code: int(code),
 		Data: data,
 		Msg:  msg,
 	})
 }
 
 func OK(data any, msg string, c *gin.Context) {
-	Result(SUCCESS, data, msg, c)
+	Result(OKCode, data, msg, c)
 }
 
 func OKWithData(data any, c *gin.Context) {
-	Result(SUCCESS, data, "成功", c)
+	Result(OKCode, data, "成功", c)
 }
 
 func OKWithMessage(msg string, c *gin.Context) {
-	Result(SUCCESS, map[string]any{}, msg, c)
+	Result(OKCode, map[string]any{}, msg, c)
 }
 
 func Fail(data any, msg string, c *gin.Context) {
-	Result(Error, data, msg, c)
+	Result(BadRequest, data, msg, c)
 }
 
 func FailWithData(data any, c *gin.Context) {
-	Result(Error, data, "成功", c)
+	Result(BadRequest, data, "成功", c)
 }
 
 func FailWithMessage(msg string, c *gin.Context) {
-	Result(Error, map[string]any{}, msg, c)
+	Result(BadRequest, map[string]any{}, msg, c)
 }
-func FailWithCode(code ErrorCode, c *gin.Context) {
-	msg, ok := ErrorMap[ErrorCode(code)]
+func FailWithCode(code Code, c *gin.Context) {
+	msg, ok := ErrorMap[Code(code)]
 	if ok {
-		Result(int(code), map[string]any{}, msg, c)
+		Result(code, map[string]any{}, msg, c)
 		return
 	}
-	Result(Error, map[string]any{}, "未知错误", c)
+	Result(code, map[string]any{}, "未知错误", c)
 }
